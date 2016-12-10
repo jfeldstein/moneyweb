6cdc236b30bbf9847644eb3c864e52b1f953c0a4
Deger Turan <deger@zenreach.com>
2016-12-10 12:46:18 -0800

    arrows and stretch

--- d3viz/script.js
+++ d3viz/script.js
@@ -4,20 +4,42 @@ var svg = d3.select("svg"),
 
 var color = d3.scaleOrdinal(d3.schemeCategory20);
 
+var manyBody = d3.forceManyBody()
+    .strength(function(node) {
+    	// debugger;
+    	return (node.group*2)*-10;
+    })
+
 var simulation = d3.forceSimulation()
     .force("link", d3.forceLink().id(function(d) { return d.id; }))
-    .force("charge", d3.forceManyBody())
-    .force("center", d3.forceCenter(width / 2, height / 2));
+    .force("charge", manyBody)
+    .force("center", d3.forceCenter(width / 3, height / 2));
+
 
 d3.json("http://localhost:8000/data_examples/lesmis.json", function(error, graph) {
   if (error) throw error;
+
+// build the arrow.
+svg.append("svg:defs").selectAll("marker")
+    .data(["end"])      // Different link/path types can be defined here
+  .enter().append("svg:marker")    // This section adds in the arrows
+    .attr("id", String)
+    .attr("viewBox", "0 -5 10 10")
+    .attr("refX", 15)
+    .attr("refY", -1.5)
+    .attr("markerWidth", 1)
+    .attr("markerHeight", 1)
+    .attr("orient", "auto")
+  .append("svg:path")
+    .attr("d", "M0,-5L10,0L0,5");
 
   var link = svg.append("g")
       .attr("class", "links")
     .selectAll("line")
     .data(graph.links)
     .enter().append("line")
-      .attr("stroke-width", function(d) { return Math.sqrt(d.value*10); });
+      .attr("stroke-width", function(d) { return Math.sqrt(d.value*10); })
+      .attr("marker-end", "url(#end)");
 
   var node = svg.append("g")
       .attr("class", "nodes")
@@ -26,7 +48,6 @@ d3.json("http://localhost:8000/data_examples/lesmis.json", function(error, graph
     .enter().append("circle")
       .attr("fill", function(d) { return color(d.group); })
        .attr("r", function(d) { return d.group[0]; })
-
       .call(d3.drag()
           .on("start", dragstarted)
           .on("drag", dragged)
