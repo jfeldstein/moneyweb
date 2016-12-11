8832a93f47ff191bdaa7872304e331a187236d0e
kkireyev <kirill.kireyev@gmail.com>
2016-12-11 12:56:31 -0800

    Fixes

--- utils/clean_json.py
+++ utils/clean_json.py
@@ -10,34 +10,53 @@ def clean_json(fname, keep_fraction = 0.5):
     import json
     orig = json.load(open(fname, 'r'))
     people_map = {}
-    people_keep = {}
+    keep = {}
     
     new = {"links": [], "nodes": []}
     
     for n in orig['nodes']:
-        if ' ' in n['id']:
-            people_keep[n['id']] = (random.random() < keep_fraction)
+        if 1 in n['group']:
+            keep[n['id']] = (random.random() < keep_fraction)
+            parts = n['id'].split(' ')
+            old_id = n['id']
             
-            if people_keep[n['id']]: 
-                parts = n['id'].split(' ')
-                if parts[-1] == 'Jr':
-                    people_map[n['id']] = n['id'] = parts[1] + ' ' + parts[0] + ' Jr'
-                else:
-                    people_map[n['id']] = n['id'] = ' '.join(parts[1:]) + ' ' + parts[0]
-                new['nodes'].append(n)
-        else:
-            new['nodes'].append(n)
+            if parts[-1] == 'Jr':
+                new_id = parts[1] + ' ' + parts[0] + ' Jr'
+            else:
+                new_id = people_map[n['id']] = ' '.join(parts[1:]) + ' ' + parts[0]
             
+            people_map[n['id']] = new_id
+    
+    
+
+
+    for n in orig['nodes']:
+        if 0 in n['group']:
+            keep[n['id']] = False
+            for l in new['links']:
+                if l['source'] == n['id'] and keep[l['target']]:
+                    keep[n['id']] = True
+                    break
+    
     for l in orig['links']:
-        if l['target'] not in people_keep:
+        if not keep[l['target']] and (l['source'] not in keep or not keep[l['source']]):
             continue
         
         if l['target'] in people_map:
+            print "%s -> %s" % (l['target'], people_map[l['target']],)
             l['target'] = people_map[l['target']]
         new['links'].append(l)
+               
+        
+    for n in orig['nodes']:
+        if keep[n['id']] == True:
+            if n['id'] in people_map:
+                n['id'] = people_map[n['id']]
+            new['nodes'].append(n)
+
         
     import json
-    with open(fname, 'w') as outfile:
+    with open(fname+'.json', 'w') as outfile:
         json.dump(new, outfile, indent=5)
             
 if __name__ == "__main__":
